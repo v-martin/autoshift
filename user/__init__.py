3a73711e928b7901ca92b85b075449376eddb655
Abo Holo <sevamartynov2003@gmail.com>
2025-10-24 18:37:42 +0300

    Remove user from all branches

--- user/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file marks the directory as a Python package
-"""
-User application for AutoShift
-"""
--- user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from .models import WorkerQualification, WorkerWarehousePreference
-
-User = get_user_model()
-
-admin.site.register(User)
-admin.site.register(WorkerQualification)
-admin.site.register(WorkerWarehousePreference)
